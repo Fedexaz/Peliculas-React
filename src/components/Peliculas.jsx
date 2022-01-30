3837bfe306c2b4e0fd500d58569ce039bf0982c9
fedexaz <fedexaz3@gmail.com>
2022-01-30 01:16:01 -0300

    Cambios en peliculas.jsx

--- src/components/Peliculas.jsx
+++ src/components/Peliculas.jsx
@@ -9,6 +9,8 @@ function Peliculas () {
 	const dispatch = useDispatch();
 	
 	let [inputPelis, setInputPelis] = useState('');
+	let [addFav, setAddFav] = useState(false);
+	let [lastAddedFav, setLastAddedFav] = useState('');
 	
 	function handleChange (e) {
 		setInputPelis (e.target.value)
@@ -18,24 +20,32 @@ function Peliculas () {
 		e.preventDefault();
 		dispatch (mostrarPeliculas (inputPelis))
 	}
+
+	function addFavourites(e){
+		setAddFav(true)
+		setLastAddedFav(e.target.id)
+		dispatch(agregarFavoritas(e.target.id))
+		setTimeout(() => { setAddFav(false) }, 3000)
+	}
 	
 	return (
 	<>
 	<h1>Películas</h1>
 	<form onSubmit={e=>handleSubmit(e)}>
-	<input type="text" value={inputPelis} onChange={e=>handleChange (e)} placeholder="Ingresa el título de la película a buscar..."/>
-	<button type="submit">Buscar</button>
+		<input type="text" value={inputPelis} onChange={e=>handleChange (e)} placeholder="Ingresa el título de la película a buscar..."/>
+		<button type="submit">Buscar</button>
 	</form>
     <hr />
-    <h1>Resultados de '{inputPelis}' ({pelis.length})</h1>
+    <h1>Resultados de '{inputPelis}'</h1>
+	{addFav ? <p>'{lastAddedFav}' agregada a Favoritos!</p> : null}
     {
 		pelis?.map(peli => {
-          return (
-            <div key={peli.imdbID}>
-            <Pelicula title={peli.Title} />
-            <button>⭐</button>
-            </div>
-          )
+        	return (
+				<div key={peli.imdbID}>
+            	<Pelicula title={peli.Title} />
+            	<button onClick={e => addFavourites(e)} id={peli.Title}>⭐</button>
+            	</div>
+          	)
         })
 	}
 	</>
